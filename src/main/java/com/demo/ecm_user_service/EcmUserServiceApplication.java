e32648004aa47591a7da3bb2e80366eedd23443f
Zayar Linn Naung <84829442+ZayarLinnNaung-Coder@users.noreply.github.com>
2024-06-08 23:01:17 +0630

    init project

---
+++ src/main/java/com/demo/ecm_user_service/EcmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ecm_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/ecm_user_service/EcmUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ecm_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
